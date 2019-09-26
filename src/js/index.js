3cf4a10c26e6de94ae1a160fcfa559679ef98f66
Bruno Li <lisiuloong@gmail.com>
2019-09-25 20:32:34 -0400

    established connection with the API call

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,19 @@
-import str from './models/Search';
-// import {add, multiply,ID} from "./views/searchView";
-import * as searchView from "./views/searchView";
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+/* eslint-disable no-alert */
+// food2fork api key: f685d6d569cfa0917bb9b449c3ee5c72
+// search url : https://www.food2fork.com/api/search
+import axios from 'axios';
 
-console.log(`using imported functions ${searchView.add(searchView.ID,2)} and ${searchView.multiply(searchView.ID,2)}. ${str}`);
+async function getResults(query) {
+  const key = 'f685d6d569cfa0917bb9b449c3ee5c72';
+  try {
+    const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+    const { recipes } = res.data;
+    console.log(recipes);
+  } catch (error) {
+    alert(error);
+  }
+}
+
+getResults('chicken');
