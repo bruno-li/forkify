a93c023dad1a99a2e419f7243e99a0e45d9bb939
Bruno Li <lisiuloong@gmail.com>
2019-10-02 11:14:47 -0400

    implemented method to truncate long recipes titles

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -16,6 +16,29 @@ export const clearResults = () => {
   elements.searchResultList.innerHTML = '';
 };
 
+// truncate recipe title
+export const limitReceipeTitle = (title, limit = 17) => {
+  const newTitle = []; // to store a new array
+
+  if (title.length > limit) {
+    //  split the title sentences with space
+    title.split(' ').reduce((acc, cur) => {
+      // checks the sum of acc and the length of the current element are < than 17
+      if (acc + cur.length <= limit) {
+        // if it is, push word into newTitle array
+        newTitle.push(cur);
+      }
+      // returns the sum of acc and the length of current element of the array
+      return acc + cur.length;
+    }, 0); // set initial accumulator value to 0
+
+    // join them back into a sentence separated by space
+    return `${newTitle.join(' ')} ...`;
+  }
+
+  return title;
+};
+
 // private function within the searchView module
 const renderRecipe = (recipe) => {
   // create a <li> element for every recipe
@@ -26,7 +49,7 @@ const renderRecipe = (recipe) => {
                             <img src="${recipe.image_url}" alt="${recipe.title}">
                         </figure>
                         <div class="results__data">
-                            <h4 class="results__name">${recipe.title}</h4>
+                            <h4 class="results__name">${limitReceipeTitle(recipe.title)}</h4>
                             <p class="results__author">${recipe.publisher}</p>
                         </div>
                       </a>
